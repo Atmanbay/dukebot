b67e4855920c89d9ef78d16cf7c5ade2e0447001
Andrew Hachten <aj@hachten.net>
2020-05-11 17:54:03 -0400

    adding error to tell user to add token value

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import Database from './database';
 import Bot from './bot/bot';
 import EventHandlers from './eventHandlers';
 
-let token = Database.get('config').find({ key: "token" });
+function main() {
+  let token = Database.get('config').find({ key: "token" }).value();
+  if (!token || !token.value) {
+    console.error('Please supply a token value in database/db.json');
+    return;
+  }
 
-let dukeBot = new Bot();
+  let dukeBot = new Bot();
 
-EventHandlers.forEach((handler) => {
-  dukeBot.registerHandler(new handler.default(Database.get('config')));
-})
+  EventHandlers.forEach((handler) => {
+    dukeBot.registerHandler(new handler.default(Database.get('config')));
+  })
 
-dukeBot.start(token.value().value);
+  dukeBot.start(token.value);
+}
+
+main();
