ce6beb5ca4142f54f29074facc45ba7913241e16
Andrew Hachten <aj@hachten.net>
2020-05-19 21:17:08 -0400

    Fixing issues caused by merge

--- src/structures/eventHandler.js
+++ src/structures/eventHandler.js
@@ -2,23 +2,8 @@ import { defaults } from 'lodash';
 
 export default class EventHandler {
   constructor() {
-    // let actual = defaults(options || {}, {
-    //   event: '',
-    //   buildServices: this.buildServices,
-    //   handle: this.handle
-    // });
-
-    // this.event = actual.event;
-    // this.handle = actual.handle;
-
-    // let me = this;
-    // let services = actual.buildServices();
-    // Object.keys(services).forEach((key) => {
-    //   me[key] = services[key];
-    // })
+    this.event = null;
   }
-  
-  buildServices() { }
 
   handle(event) { }
 }
