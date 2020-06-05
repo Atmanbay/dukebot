cc06768b8e2f0e974e8bd8a4e558259592f5bc24
Andrew Hachten <aj@hachten.net>
2020-06-05 17:49:07 -0400

    adding christian command to remind heathens

---
+++ src/commands/christian.js
@@ -0,0 +1,15 @@
+import Command from '../objects/command';
+
+export default class AliveCommand extends Command {
+  constructor() {
+    super();
+    this.details = {
+      name: 'christian',
+      description: 'Helpful reminder',
+    };
+  }
+
+  execute(message) {
+    message.channel.send("https://i.redd.it/nq4wp5oi6xky.jpg");
+  }
+}
