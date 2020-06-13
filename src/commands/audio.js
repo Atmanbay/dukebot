e404f41721e8d34fd5b865cc51508f7d2d0af023
Andrew Hachten <aj@hachten.net>
2020-06-13 10:51:10 -0400

    adding audio clip uploading

--- src/commands/audio.js
+++ src/commands/audio.js
@@ -1,5 +1,7 @@
 import Command from '../objects/command';
 import fs from 'fs';
+import download from 'download';
+import sanitize from 'sanitize-filename';
 
 export default class AudioCommand extends Command {
   constructor(container) {
@@ -21,8 +23,13 @@ export default class AudioCommand extends Command {
           optional: true
         },
         {
-          name: 'l',
+          name: 'list',
           description: 'Flag to list all audio clips',
+          optional: true
+        },
+        {
+          name: 'upload',
+          description: 'Flag to start the audio clip upload process',
           optional: true
         }
       ]
@@ -30,11 +37,34 @@ export default class AudioCommand extends Command {
   }
 
   execute(message, args) {
-    if (args.l) {
+    if (args.list) {
       let clips = this.audioService.getClips();
       clips.unshift('```');
       clips.push('```');
       message.channel.send(clips);
+      return;
+    }
+
+    if (args.upload) {
+      message.channel.send('Please upload your file!').then(() => {
+        let filter = m => message.author.id === m.author.id;
+        message.channel.awaitMessages(filter, { time: 60000, max: 1, errors: ['time'] })
+          .then(messages => {
+            let message = messages.first();
+            let attachments = message.attachments;
+            if (attachments && attachments.size === 1) {
+              let url = attachments.first().url;
+              let options = {};
+              if (message.content) {
+                options.filename = `${sanitize(message.content)}.mp3`;
+              }
+              download(url, this.configService.directories.audio, options);
+            }
+          })
+          .catch(() => {
+            message.channel.send('You took too long!');
+          });
+      });
       return;
     }
 
