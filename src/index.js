0ec6923a0755347145fee5534268d94502c06176
Andrew Hachten <aj@hachten.net>
2020-07-05 13:46:21 -0400

    initial commit of babel build functionality

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import Bot from './bot/bot';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
 function main() {
   let dukeBot = new Bot();
--- src/services/configService.js
+++ src/services/configService.js
@@ -1,5 +1,5 @@
 import { merge } from 'lodash';
-import config from '../../config.js';
+import config from '../config.js';
 
 // Just a fancy wrapper for the config.js file at the project's root dir
 export default class ConfigService {
