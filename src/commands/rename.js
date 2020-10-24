15b13e5e00f087afeb3a970649d2308b2fbe101c
Andrew <ajhachten@gmail.com>
2020-10-24 14:13:28 -0400

    Fixing a bug with define command

--- src/commands/rename.js
+++ src/commands/rename.js
@@ -1,27 +1,25 @@
 import Command from "../objects/command";
-import joi from 'joi';
+import joi from "joi";
 
 export default class RenameCommand extends Command {
   constructor(container) {
     super();
     this.botUserService = container.botUserService;
     this.details = {
-      name: 'rename',
-      description: 'Rename the bot',
-      args: joi.object({
-        text: joi
-          .string()
-          .required()
-          .note('New name for the bot')
-      })
-        .rename('t', 'text')
+      name: "rename",
+      description: "Rename the bot",
+      args: joi
+        .object({
+          text: joi.string().required().note("New name for the bot"),
+        })
+        .rename("t", "text"),
     };
   }
-  
+
   async execute(message, args) {
     let botUserId = this.botUserService.getBotUser().id;
     let guildUser = await message.guild.members.fetch(botUserId);
 
-    guildUser.setNickname(args.n)
+    guildUser.setNickname(args.text);
   }
 }
