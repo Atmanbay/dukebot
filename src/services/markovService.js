1680224cb22e5817e294af261b591ebb47530475
Andrew <ajhachten@gmail.com>
2020-10-24 14:03:26 -0400

    Removing debugging code

--- src/services/markovService.js
+++ src/services/markovService.js
@@ -1,29 +1,28 @@
-import Markov from 'markov-strings';
+import Markov from "markov-strings";
 
 export default class MarkovService {
   constructor(container) {
     this.loggerService = container.loggerService;
   }
 
-  buildMarkov({messages, stateSize, maxTries, variance}) {
-    return 'this is a markov';
-    // let markov = new Markov(messages, { stateSize: stateSize });
-    // markov.buildCorpus();
+  buildMarkov({ messages, stateSize, maxTries, variance }) {
+    let markov = new Markov(messages, { stateSize: stateSize });
+    markov.buildCorpus();
 
-    // let options = {
-    //   maxTries: maxTries,
-    //   prng: Math.random,
-    //   filter: (result) => {
-    //     return result.refs.length > variance && result.string.length <= 2000
-    //   }
-    // };
+    let options = {
+      maxTries: maxTries,
+      prng: Math.random,
+      filter: (result) => {
+        return result.refs.length > variance && result.string.length <= 2000;
+      },
+    };
 
-    // try {
-    //   let result = markov.generate(options);
-    //   return result.string;
-    // } catch (error) {
-    //   this.loggerService.error(error);
-    //   return 'Failed to build Markov';
-    // }
+    try {
+      let result = markov.generate(options);
+      return result.string;
+    } catch (error) {
+      this.loggerService.error(error);
+      return "Failed to build Markov";
+    }
   }
 }
